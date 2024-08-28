951ce80847cbca11ae5d63d57125a46562d983cc
Lucas Arantes <108706950+Arantees@users.noreply.github.com>
2024-08-28 19:41:41 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,3 +1,13 @@
-<?php
-phpinfo();  // Exibe informações sobre a instalação do PHP
-?>
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Aula do Orlando
+</body>
+</html>
+
